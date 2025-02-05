c1f1650915065ecf0a8ee75235232a8c04376bac
utkarsh-singh1 <utkarsh.singh1@india.nec.com>
2025-02-05 15:32:08 +0530

    Remove JS files

--- go/bankServer/frontend/src/types/auth_state.ts
+++
@@ -1,7 +0,0 @@
-import type { User } from './user'
-
-export interface AuthState {
-  user: User | null
-  accessToken: string | null
-  refreshToken: string | null
-}
--- go/bankServer/frontend/src/types/user.ts
+++
@@ -1,5 +0,0 @@
-export interface User {
-  username: string
-  full_name: string
-  email: string
-}
